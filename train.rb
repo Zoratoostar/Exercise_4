0dd9294e787a18de589debb8972e0f6fbbf101bb
Zoratoostar <toevgeny@gmail.com>
2017-10-25 13:03:49 +0800

    Lesson 4

--- train.rb
+++ train.rb
@@ -90,29 +90,3 @@ class CargoTrain < Train
     end
   end
 end
-
-class PassengerCarriage
-  attr_reader :number, :seats
-
-  def initialize(number, seats)
-    @number = number.to_s
-    @seats = seats.to_i
-  end
-
-  def to_s
-    number + '::' + seats.to_s + '::'
-  end
-end
-
-class FreightWagon
-  attr_reader :number, :purpose
-
-  def initialize(number, purpose)
-    @number = number.to_s
-    @purpose = purpose.to_s
-  end
-
-  def to_s
-    number + '=' + purpose + '='
-  end
-end
